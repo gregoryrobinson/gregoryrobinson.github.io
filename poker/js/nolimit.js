978d83720bea5bea6477e06af04bd7d633bf45cb
gregoryrobinson <35582627+gregoryrobinson@users.noreply.github.com>
2019-02-15 16:58:32 -0800

    Add files via upload

--- poker/js/nolimit.js
+++ poker/js/nolimit.js
@@ -156,7 +156,7 @@ function checkHand(hand) {
             }
             $('#strategy').html(strategy);
             $('#extras').show();
-            $('#details').hide();
+            $('#details').show();
         }
     }
 }
